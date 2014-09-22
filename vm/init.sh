74a89ec5c87c8a811b04d780951db1b53f551d64
Dave J <contact_davej@ymail.com>
2014-09-22 10:50:12 +0200

    Update init.sh

--- vm/init.sh
+++ vm/init.sh
@@ -2,5 +2,5 @@
 base="http://github.com/DaveAtGit/arch_config/raw/master/vm/"
 curl -o arch_init.sh "${base}arch_init.sh"
 curl -o arch_init_chroot.sh "${base}arch_init_chroot.sh"
-chmod u+x arch_init*
+chmod u+x arch_init{,_chroot}.sh
 ./arch_init.sh
