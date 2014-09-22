329f6db5c744610e644ef63f0deed370e3f5a644
Dave J <contact_davej@ymail.com>
2014-09-22 10:35:50 +0200

    Create arch_init.sh

---
+++ vm/arch_init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#-- Init-script for arch-linux-vms.
+
+#-- Partitioning disk with default size, naming it
+#   "archlinux" and formatting it to ext4.
+
+# !! -A 1:set:2 should switch bootable, but it seems
+#    not possible with sgdisk. And it seems to use the
+#    wrong table-format...
+#sgdisk -n 1:2048:0 -c 1:"archlinux" -t 1:8300 /dev/sda
+cfdisk /dev/sda     # !! currently requires user-interaction...
+mkfs.ext4 -L archlinux /dev/sda1
+
+#-- Mounting and base-installation
+mount /dev/sda1 /mnt
+pacstrap /mnt base
+genfstab -p /mnt >> /mnt/etc/fstab
+arch-chroot /mnt arch_init_chroot.sh
+
+#-- Unmounting and restarting
+umount /mnt
+reboot
